8244823bf8403a75604bc568cadb94776f7ed9f8
nkanchi7946 <73675290+nkanchi7946@users.noreply.github.com>
2023-03-22 15:10:01 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,4 +5,5 @@ streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale,Spinach and Rocket Smoothy')
+streamlit.text('Kale,Spinach and Rocket Smoothie')
+streamlit.text('Hard-Bioled Free-Range Eggs')
